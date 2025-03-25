41695fb9c032c2d0c79c421a781d7d2256ed5d1e
RayJay1108 <rjvargas0804@gmail.com>
2025-03-24 21:35:36 -0600

    finally got the text to show, also fix some html semantics

--- joke.js
+++ joke.js
@@ -12,6 +12,6 @@ document.getElementById('programming').addEventListener('click', () => {
     .then(response => response.json())
     .then(data => {
         console.log(data);
-        
+        document.getElementById('joke').textContent = data.joke || data.setup +"..."+ data.delivery;
     })
 })
