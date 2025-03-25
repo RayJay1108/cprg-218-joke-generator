9b6e0128730c257dc2a8f9e91f7346831616b540
RayJay1108 <rjvargas0804@gmail.com>
2025-03-24 21:41:05 -0600

    deadass just copy and pasted it then replaced it with misc:)
    wow this is prolly the wrong way but whatevs it works

--- joke.js
+++ joke.js
@@ -6,6 +6,8 @@ fetch('https://v2.jokeapi.dev/joke/Any?safe-mode')
     joke.textContent = data.joke || data.setup +"..."+ data.delivery;
 }); 
 
+// -----------> Programming Jokes <-----------//
+
 document.getElementById('programming').addEventListener('click', () => {
     fetch('https://v2.jokeapi.dev/joke/Programming?safe-mode')
     .then(response => response.json())
@@ -15,4 +17,17 @@ document.getElementById('programming').addEventListener('click', () => {
     })
 
     document.getElementById('title').textContent = "A Random Programming Joke!"; 
+})
+
+// -----------> Misc Jokes <-----------//
+
+document.getElementById('misc').addEventListener('click', () => {
+    fetch('https://v2.jokeapi.dev/joke/Miscellaneous?safe-mode')
+    .then(response => response.json())
+    .then(data => {
+        console.log(data);
+        document.getElementById('joke').textContent = data.joke || data.setup +"..."+ data.delivery;
+    })
+
+    document.getElementById('title').textContent = "A Random Misc Joke!"; 
 })
